0ba45bec0cbdbda3976459985a411ae1a1660bd5
salok0282 <salok0282@gmail.com>
2016-12-08 12:06:31 +0530

    committing changes

--- LinqRestrictionOperators/LinqRestrictionOperators/Program.cs
+++ LinqRestrictionOperators/LinqRestrictionOperators/Program.cs
@@ -17,16 +17,12 @@ namespace LinqRestrictionOperators
             //Uncomment the method calls below that you want to run or execute
 
 
-            //objSamples.RestrictionOperatorExaple1(); // this method uses of where clause to find all elements of an array with a value greater than 6.
+            //objSamples.RestrictionOperatorExaple1(); // Uses of where clause to find all elements of an array with a value greater than 6.
 
-            objSamples.RestrictionOperatorExaple2(); // This sample uses the where clause to find all products that are out of stock
+            //objSamples.RestrictionOperatorExaple2(); // Uses of where clause to find all products that have out of stock.
 
-           // objSamples.RestrictionOperatorExaple3(); // This sample uses the where clause to find all products that are in  stock and cost 
-            // more than 3.00 per unit
+            objSamples.RestrictionOperatorExaple3(); // Uses of where clause to find all products that are in stock and Category is Beverages.
 
-            //objSamples.RestrictionOperatorExaple4();; // This sample uses the where  clause to find all customers in Washington and then it 
-            // uses a foreach loop to iterate over the orders collection that belongs to each 
-            // customer
 
             //objSamples.RestrictionOperatorExaple5();; // This sample demonstrates an indexed where clause that returns digits whose name is 
             // shorter than their value
@@ -37,85 +33,63 @@ namespace LinqRestrictionOperators
 
         public class LinqRestrictionOperatorSamples
         {
-            private List<Product> productList;
-            private List<Customer> customerList;
-
+            private List<Product> productList; // i have created product class and created product on it
+            
             [Description("This sample uses of where clause to find all elements of an array with a value greater than 6.")]
             public void RestrictionOperatorExaple1()
             {
                 int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-                var lowNums =
+                var greaterNums =
                     from num in numbers
                     where num > 6
                     select num;
 
                 Console.WriteLine("Numbers > 6:");
-                foreach (var x in lowNums)
+                foreach (var n in greaterNums)
                 {
-                    Console.WriteLine(x);
+                    Console.WriteLine(n);
                 }
                 Console.ReadLine();
             }
 
-            [Description("This sample uses the where clause to find all products that are out of stock.")]
+            [Description("Uses of where clause to find all products that have out of stock.")]
             public void RestrictionOperatorExaple2()
             {
                 List<Product> products = GetProductList();
 
-                var soldOutProducts =
-                    from prod in products
-                    where prod.UnitsInStock == 0
-                    select prod;
+                var productsSoldOut =
+                    from p in products
+                    where p.UnitsInStock == 0
+                    select p;
 
-                Console.WriteLine("Sold out products:");
-                foreach (var product in soldOutProducts)
+                Console.WriteLine("products list that are Sold out :");
+                foreach (var pd in productsSoldOut)
                 {
-                    Console.WriteLine("{0} is sold out!", product.ProductName);
+                    Console.WriteLine("{0} is sold out!", pd.ProductName);
                 }
                 Console.ReadLine();
             }
 
-            [Description("This sample uses the where clause to find all products that are in stock and cost more than 3.00 per unit.")]
+            [Description("Uses of where clause to find all products that are in stock and Category is Beverages.")]
             public void RestrictionOperatorExaple3()
             {
                 List<Product> products = GetProductList();
 
-                var expensiveInStockProducts =
-                    from prod in products
-                    where prod.UnitsInStock > 0 && prod.UnitPrice > 3.00M
-                    select prod;
+                var BeveragesInStockProducts =
+                    from p in products
+                    where p.UnitsInStock > 0 && p.Category == "Beverages" && p.UnitPrice>5
+                    select p;
 
-                Console.WriteLine("In-stock products that cost more than 3.00:");
-                foreach (var product in expensiveInStockProducts)
+                Console.WriteLine("In-stock products that Category is Beverages:");
+                foreach (var pd in BeveragesInStockProducts)
                 {
-                    Console.WriteLine("{0} is in stock and costs more than 3.00.", product.ProductName);
+                    Console.WriteLine("{0} is in stock and Category is Beverages.", pd.ProductName);
                 }
                 Console.ReadLine();
             }
 
-            [Description("This sample uses the where clause to find all customers in Washington and then it uses a foreach loop to iterate over the orders collection that belongs to each customer.")]
-            public void RestrictionOperatorExaple4()
-            {
-                List<Customer> customers = GetCustomerList();
-
-                var waCustomers =
-                    from cust in customers
-                    where cust.Region == "WA"
-                    select cust;
-
-                Console.WriteLine("Customers from Washington and their orders:");
-                foreach (var customer in waCustomers)
-                {
-                    Console.WriteLine("Customer {0}: {1}", customer.CustomerID, customer.CompanyName);
-                    foreach (var order in customer.Orders)
-                    {
-                        Console.WriteLine("  Order {0}: {1}", order.OrderID, order.OrderDate);
-                    }
-                }
-                Console.ReadLine();
-            }
-
+          
             [Description("This sample demonstrates an indexed where clause that returns digits whose name is shorter than their value.")]
             public void RestrictionOperatorExaple5()
             {
@@ -138,22 +112,15 @@ namespace LinqRestrictionOperators
 
                 return productList;
             }
-
-            public List<Customer> GetCustomerList()
-            {
-                if (customerList == null)
-                    createLists();
-
-                return customerList;
-            }
+            
 
             private void createLists()
             {
                 // Product data created in-memory using collection initializer:
                 productList =
                     new List<Product> {
-                    new Product { ProductID = 1, ProductName = "Chai", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 39 },
-                    new Product { ProductID = 2, ProductName = "Chang", Category = "Beverages", UnitPrice = 19.0000M, UnitsInStock = 17 },
+                    new Product { ProductID = 1, ProductName = "Aam panna", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 39 },
+                    new Product { ProductID = 2, ProductName = "Apong", Category = "Beverages", UnitPrice = 19.0000M, UnitsInStock = 17 },
                     new Product { ProductID = 3, ProductName = "Aniseed Syrup", Category = "Condiments", UnitPrice = 10.0000M, UnitsInStock = 13 },
                     new Product { ProductID = 4, ProductName = "Chef Anton's Cajun Seasoning", Category = "Condiments", UnitPrice = 22.0000M, UnitsInStock = 53 },
                     new Product { ProductID = 5, ProductName = "Chef Anton's Gumbo Mix", Category = "Condiments", UnitPrice = 21.3500M, UnitsInStock = 0 },
@@ -175,7 +142,7 @@ namespace LinqRestrictionOperators
                     new Product { ProductID = 21, ProductName = "Sir Rodney's Scones", Category = "Confections", UnitPrice = 10.0000M, UnitsInStock = 3 },
                     new Product { ProductID = 22, ProductName = "Gustaf's Knäckebröd", Category = "Grains/Cereals", UnitPrice = 21.0000M, UnitsInStock = 104 },
                     new Product { ProductID = 23, ProductName = "Tunnbröd", Category = "Grains/Cereals", UnitPrice = 9.0000M, UnitsInStock = 61 },
-                    new Product { ProductID = 24, ProductName = "Guaraná Fantástica", Category = "Beverages", UnitPrice = 4.5000M, UnitsInStock = 20 },
+                    new Product { ProductID = 24, ProductName = "Coconut water", Category = "Beverages", UnitPrice = 4.5000M, UnitsInStock = 20 },
                     new Product { ProductID = 25, ProductName = "NuNuCa Nuß-Nougat-Creme", Category = "Confections", UnitPrice = 14.0000M, UnitsInStock = 76 },
                     new Product { ProductID = 26, ProductName = "Gumbär Gummibärchen", Category = "Confections", UnitPrice = 31.2300M, UnitsInStock = 15 },
                     new Product { ProductID = 27, ProductName = "Schoggi Schokolade", Category = "Confections", UnitPrice = 43.9000M, UnitsInStock = 49 },
@@ -185,16 +152,16 @@ namespace LinqRestrictionOperators
                     new Product { ProductID = 31, ProductName = "Gorgonzola Telino", Category = "Dairy Products", UnitPrice = 12.5000M, UnitsInStock = 0 },
                     new Product { ProductID = 32, ProductName = "Mascarpone Fabioli", Category = "Dairy Products", UnitPrice = 32.0000M, UnitsInStock = 9 },
                     new Product { ProductID = 33, ProductName = "Geitost", Category = "Dairy Products", UnitPrice = 2.5000M, UnitsInStock = 112 },
-                    new Product { ProductID = 34, ProductName = "Sasquatch Ale", Category = "Beverages", UnitPrice = 14.0000M, UnitsInStock = 111 },
-                    new Product { ProductID = 35, ProductName = "Steeleye Stout", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 20 },
+                    new Product { ProductID = 34, ProductName = "Fruit jump", Category = "Beverages", UnitPrice = 14.0000M, UnitsInStock = 111 },
+                    new Product { ProductID = 35, ProductName = "Gajjar ka doodh", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 20 },
                     new Product { ProductID = 36, ProductName = "Inlagd Sill", Category = "Seafood", UnitPrice = 19.0000M, UnitsInStock = 112 },
                     new Product { ProductID = 37, ProductName = "Gravad lax", Category = "Seafood", UnitPrice = 26.0000M, UnitsInStock = 11 },
-                    new Product { ProductID = 38, ProductName = "Côte de Blaye", Category = "Beverages", UnitPrice = 263.5000M, UnitsInStock = 17 },
-                    new Product { ProductID = 39, ProductName = "Chartreuse verte", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 69 },
+                    new Product { ProductID = 38, ProductName = "Jal-jeera", Category = "Beverages", UnitPrice = 263.5000M, UnitsInStock = 17 },
+                    new Product { ProductID = 39, ProductName = "Kala Khatta", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 69 },
                     new Product { ProductID = 40, ProductName = "Boston Crab Meat", Category = "Seafood", UnitPrice = 18.4000M, UnitsInStock = 123 },
                     new Product { ProductID = 41, ProductName = "Jack's New England Clam Chowder", Category = "Seafood", UnitPrice = 9.6500M, UnitsInStock = 85 },
                     new Product { ProductID = 42, ProductName = "Singaporean Hokkien Fried Mee", Category = "Grains/Cereals", UnitPrice = 14.0000M, UnitsInStock = 26 },
-                    new Product { ProductID = 43, ProductName = "Ipoh Coffee", Category = "Beverages", UnitPrice = 46.0000M, UnitsInStock = 17 },
+                    new Product { ProductID = 43, ProductName = "Kulukki sarbath", Category = "Beverages", UnitPrice = 46.0000M, UnitsInStock = 17 },
                     new Product { ProductID = 44, ProductName = "Gula Malacca", Category = "Condiments", UnitPrice = 19.4500M, UnitsInStock = 27 },
                     new Product { ProductID = 45, ProductName = "Rogede sild", Category = "Seafood", UnitPrice = 9.5000M, UnitsInStock = 5 },
                     new Product { ProductID = 46, ProductName = "Spegesild", Category = "Seafood", UnitPrice = 12.0000M, UnitsInStock = 95 },
@@ -218,44 +185,20 @@ namespace LinqRestrictionOperators
                     new Product { ProductID = 64, ProductName = "Wimmers gute Semmelknödel", Category = "Grains/Cereals", UnitPrice = 33.2500M, UnitsInStock = 22 },
                     new Product { ProductID = 65, ProductName = "Louisiana Fiery Hot Pepper Sauce", Category = "Condiments", UnitPrice = 21.0500M, UnitsInStock = 76 },
                     new Product { ProductID = 66, ProductName = "Louisiana Hot Spiced Okra", Category = "Condiments", UnitPrice = 17.0000M, UnitsInStock = 4 },
-                    new Product { ProductID = 67, ProductName = "Laughing Lumberjack Lager", Category = "Beverages", UnitPrice = 14.0000M, UnitsInStock = 52 },
+                    new Product { ProductID = 67, ProductName = "Kesar kasturi", Category = "Beverages", UnitPrice = 14.0000M, UnitsInStock = 52 },
                     new Product { ProductID = 68, ProductName = "Scottish Longbreads", Category = "Confections", UnitPrice = 12.5000M, UnitsInStock = 6 },
                     new Product { ProductID = 69, ProductName = "Gudbrandsdalsost", Category = "Dairy Products", UnitPrice = 36.0000M, UnitsInStock = 26 },
-                    new Product { ProductID = 70, ProductName = "Outback Lager", Category = "Beverages", UnitPrice = 15.0000M, UnitsInStock = 15 },
+                    new Product { ProductID = 70, ProductName = "Kokum sarbat", Category = "Beverages", UnitPrice = 15.0000M, UnitsInStock = 15 },
                     new Product { ProductID = 71, ProductName = "Flotemysost", Category = "Dairy Products", UnitPrice = 21.5000M, UnitsInStock = 26 },
                     new Product { ProductID = 72, ProductName = "Mozzarella di Giovanni", Category = "Dairy Products", UnitPrice = 34.8000M, UnitsInStock = 14 },
                     new Product { ProductID = 73, ProductName = "Röd Kaviar", Category = "Seafood", UnitPrice = 15.0000M, UnitsInStock = 101 },
                     new Product { ProductID = 74, ProductName = "Longlife Tofu", Category = "Produce", UnitPrice = 10.0000M, UnitsInStock = 4 },
-                    new Product { ProductID = 75, ProductName = "Rhönbräu Klosterbier", Category = "Beverages", UnitPrice = 7.7500M, UnitsInStock = 125 },
-                    new Product { ProductID = 76, ProductName = "Lakkalikööri", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 57 },
+                    new Product { ProductID = 75, ProductName = "Laopani", Category = "Beverages", UnitPrice = 7.7500M, UnitsInStock = 125 },
+                    new Product { ProductID = 76, ProductName = "Thandai", Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 57 },
                     new Product { ProductID = 77, ProductName = "Original Frankfurter grüne Soße", Category = "Condiments", UnitPrice = 13.0000M, UnitsInStock = 32 }
                 };
 
-                // Customer/Order data read into memory from XML file using XLinq:
-                customerList = (
-                    from e in XDocument.Load("Customers.xml").Root.Elements("customer")
-                    select new Customer
-                    {
-                        CustomerID = (string)e.Element("id"),
-                        CompanyName = (string)e.Element("name"),
-                        Address = (string)e.Element("address"),
-                        City = (string)e.Element("city"),
-                        Region = (string)e.Element("region"),
-                        PostalCode = (string)e.Element("postalcode"),
-                        Country = (string)e.Element("country"),
-                        Phone = (string)e.Element("phone"),
-                        Fax = (string)e.Element("fax"),
-                        Orders = (
-                            from o in e.Elements("orders").Elements("order")
-                            select new Order
-                            {
-                                OrderID = (int)o.Element("id"),
-                                OrderDate = (DateTime)o.Element("orderdate"),
-                                Total = (decimal)o.Element("total")
-                            })
-                            .ToArray()
-                    })
-                    .ToList();
+               
             }
         }
         public class Product
@@ -267,25 +210,6 @@ namespace LinqRestrictionOperators
             public int UnitsInStock { get; set; }
         }
 
-        public class Order
-        {
-            public int OrderID { get; set; }
-            public DateTime OrderDate { get; set; }
-            public decimal Total { get; set; }
-        }
-
-        public class Customer
-        {
-            public string CustomerID { get; set; }
-            public string CompanyName { get; set; }
-            public string Address { get; set; }
-            public string City { get; set; }
-            public string Region { get; set; }
-            public string PostalCode { get; set; }
-            public string Country { get; set; }
-            public string Phone { get; set; }
-            public string Fax { get; set; }
-            public Order[] Orders { get; set; }
-        }
+        
     }
 }
